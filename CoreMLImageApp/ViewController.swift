55a2aaeb9a65f887d0e405b7be5e28bfe9310b07
tatendakabike <tkabike@gmail.com>
2019-12-13 02:18:57 +0200

    Initial Commit

---
+++ CoreMLImageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMLImageApp
+//
+//  Created by Tatenda Kabike on 2019/12/13.
+//  Copyright © 2019 Tatenda Kabike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
